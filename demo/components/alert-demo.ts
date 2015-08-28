ed35a1297bdabbdc1e7838b697646b15ee2eb5c1
Dmitriy Schekhovtsov <valorkin@gmail.com>
2015-08-29 00:03:24 +0300

    Docs: added readme for alert

--- demo/components/alert-demo.ts
+++ demo/components/alert-demo.ts
@@ -4,18 +4,32 @@ import {Component, View, bootstrap, CORE_DIRECTIVES} from 'angular2/angular2';
 
 import {Alert} from '../../components/index';
 
+// webpack html imports
+let doc = require('../../components/alert/readme.md');
+
 @Component({
   selector: 'alert-demo'
 })
 @View({
   template: `
-    <br><hr/>
-    <h2>Alerts demo</h2>
-    <span *ng-if="name">Hello, {{name}}!</span>
-    <alert dismiss-on-timeout="3000">This alert will dismiss in 3s</alert>
-    <alert *ng-for="#alert of alerts;#i = index" [type]="alert.type" (close)="closeAlert(i)">{{ alert.msg }}</alert>
-    <div></div>
-    <button type="button" class='btn btn-primary' (click)="addAlert()">Add Alert</button>
+  <section id="alert">
+    <div class="page-header">
+      <h1>
+        Alert
+        <small>(<a href="https://github.com/valor-software/ng2-bootstrap/blob/master/components/alert/alert.ts">src</a>)</small>
+      </h1>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <span *ng-if="name">Hello, {{name}}!</span>
+        <alert dismiss-on-timeout="3000">This alert will dismiss in 3s</alert>
+        <alert *ng-for="#alert of alerts;#i = index" [type]="alert.type" (close)="closeAlert(i)">{{ alert.msg }}</alert>
+        <div></div>
+        <button type="button" class='btn btn-primary' (click)="addAlert()">Add Alert</button>
+      </div>
+      <div class="col-md-6">${doc}</div>
+    </div>
+  </section>
   `,
   directives: [Alert, CORE_DIRECTIVES]
 })
